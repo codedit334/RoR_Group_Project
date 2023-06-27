502767cadedf6db9a3c0df27a6d9bd25c70266f0
Gmanlove <emmagmanc@gmail.com>
2023-06-27 12:32:27 +0100

    Add Recipes list

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
   has_many :foods, class_name: 'Food', foreign_key: 'user_id'
   validates :name, presence: true
+  def confirmed?
+    true
+  end
 end
