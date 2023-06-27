4115098c38a84e871191d2757cfff521d63ed4da
Gmanlove <emmagmanc@gmail.com>
2023-06-27 22:30:40 +0100

    Add Public recipe list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Add the following route for signing out
+  resources :users, only: [:show]
 
   resources :recipes, except: [:update]
-  resources :foods, except: [:update]
+  resources :public_recipes, except: [:update]
 
-  root to: 'recipes#index'
+  get '/public_recipes', to: 'public_recipes#index'
+
+  # Defines the root path route ("/")
+  root 'public_recipes#index'
 end
