38a28c015a2c120be40428877055302be40eb137
Emmanuel OBI <emmagmanc@gmail.com>
2023-06-27 19:55:05 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   layout 'standard'
   include Devise::Controllers::Helpers
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -7,15 +6,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
- Recipes-list
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   helper_method :sort_column, :sort_direction
   include ApplicationHelper
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 end
