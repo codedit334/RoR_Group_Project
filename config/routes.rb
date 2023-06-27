30a08a9fb392863efbb0db4bde1fa52a1baeef9c
codedit334 <oussama7elabdioui@gmail.com>
2023-06-27 12:56:27 +0100

    Create food controller actions; Add food views; Add sort feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/edit'
+  get 'foods/destroy'
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
   # root to: "home#index"
+
+  resources :foods, except: [:update]
 end
