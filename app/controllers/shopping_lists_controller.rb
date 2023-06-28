dbe7d80daaa40c6e69747689595a04aa7905e987
codedit334 <oussama7elabdioui@gmail.com>
2023-06-28 16:27:34 +0100

    Refactor code

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -11,11 +11,25 @@ class ShoppingListsController < ApplicationController
 
   private
 
+  # def calculate_missing_foods
+  #   recipe_foods = @recipes.map { |recipe| recipe.foods }
+  #   general_food_ids = @general_food_list.pluck(:id)
+  #   missing_food_ids = recipe_foods.flatten.reject { |food| general_food_ids.include?(food.id) }
+  #   Food.where(id: missing_food_ids)
+  # end
+
   def calculate_missing_foods
-    recipe_foods = @recipes.map { |recipe| recipe.foods }
-    general_food_ids = @general_food_list.pluck(:id)
-    missing_food_ids = recipe_foods.flatten.reject { |food| general_food_ids.include?(food.id) }
-    Food.where(id: missing_food_ids)
+    general_food_quantities = @general_food_list.group(:name).sum(:quantity)
+    recipe_food_quantities = @recipes.flat_map { |recipe| recipe.foods.group(:name).sum(:quantity) }
+  
+    missing_foods = {}
+    recipe_food_quantities.each do |name, quantity|
+      if general_food_quantities[name].to_i < quantity
+        missing_foods[name] = quantity - general_food_quantities[name].to_i
+      end
+    end
+  
+    missing_foods
   end
 
   def calculate_total_count
